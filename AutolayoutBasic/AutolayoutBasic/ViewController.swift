33e08e7045ec4416ed5883f85a7f69674dc2e562
Balu Naik <balu.naik@namshi.com>
2020-04-22 07:41:39 +0400

    Autolayout basic

---
+++ AutolayoutBasic/AutolayoutBasic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutolayoutBasic
+//
+//  Created by Balu Naik on 4/22/20.
+//  Copyright © 2020 BaluTutorial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
