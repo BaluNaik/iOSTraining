06e14bbba9487808d5407869a8425769a8ab2195
Balu Naik <balu.naik@namshi.com>
2020-04-22 06:15:56 +0400

    Implemented final view with data

--- FormApplicationWithProtocal/FormApplicationWithProtocal/ViewController.swift
+++ FormApplicationWithProtocal/FormApplicationWithProtocal/ViewController.swift
@@ -11,10 +11,17 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var button: BTButton!
+    @IBOutlet weak var fullNameLabel: BTLabel!
+    @IBOutlet weak var emailLabel: BTLabel!
+    @IBOutlet weak var genderLabel: BTLabel!
+    @IBOutlet weak var dobLabel: BTLabel!
+    @IBOutlet weak var infoView: UIView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         button.showEnabled()
+        self.infoView.isHidden = true
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -31,8 +38,23 @@ class ViewController: UIViewController {
 extension ViewController: FormViewControllerDelegate {
     
     func submitDate(data: Emp) {
-        // Now data will get in data parameter
-        
+        if let firstName = data.fName, let lastName = data.lName {
+            self.fullNameLabel.text = "Full Name: \(firstName) \(lastName)"
+            self.fullNameLabel.halfTextColorChange(self.fullNameLabel.text ?? "", "\(firstName) \(lastName)")
+        }
+        if let email = data.email {
+            self.emailLabel.text = "Email: \(email)"
+            self.emailLabel.halfTextColorChange(self.emailLabel.text ?? "" , "\(email)")
+        }
+        if let gender = data.gender {
+            self.genderLabel.text = "Gender: \(gender)"
+            self.genderLabel.halfTextColorChange(self.genderLabel.text ?? "" , "\(gender)")
+        }
+        if let dob = data.dob {
+            self.dobLabel.text = "DOB: \(dob)"
+            self.dobLabel.halfTextColorChange(self.dobLabel.text ?? "", "\(dob)")
+        }
+        self.infoView.isHidden = false
     }
     
 }
