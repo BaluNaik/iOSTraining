36e7883e5e4d57c4c3db6b33ebf76a6d34978ab9
Balu Naik <balu.naik@namshi.com>
2020-06-08 06:21:12 +0400

    Updated key reading process

--- BTGoogleMaps/BTGoogleMaps/AppDelegate.swift
+++ BTGoogleMaps/BTGoogleMaps/AppDelegate.swift
@@ -17,9 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        GMSServices.provideAPIKey("AIzaSyADFyAXegqyXpxZlhR8k0o_PcjtFrCWDFQ")
-        GMSPlacesClient.provideAPIKey("AIzaSyADFyAXegqyXpxZlhR8k0o_PcjtFrCWDFQ")
-        
+        //GMSServices.provideAPIKey("AIzaSyADFyAXegqyXpxZlhR8k0o_PcjtFrCWDFQ")
+        //GMSPlacesClient.provideAPIKey("AIzaSyADFyAXegqyXpxZlhR8k0o_PcjtFrCWDFQ")
+        readPropertyList()
         return true
     }
 
@@ -37,5 +37,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    func readPropertyList() {
+        var propertyListFormat =  PropertyListSerialization.PropertyListFormat.xml
+        var plistData: [String: AnyObject] = [:]
+        if let plistPath = Bundle.main.path(forResource: "Info", ofType: "plist"),
+            let plistXML = FileManager.default.contents(atPath: plistPath) {
+            do {
+                plistData = try PropertyListSerialization.propertyList(from: plistXML, options: .mutableContainersAndLeaves, format: &propertyListFormat) as! [String:AnyObject]
+                if let googlekey = plistData["GoogleKey"] as? String {
+                    print(googlekey)
+                    GMSServices.provideAPIKey(googlekey)
+                    GMSPlacesClient.provideAPIKey(googlekey)
+                }
+            } catch {
+                print("Error reading plist: \(error), format: \(propertyListFormat)")
+            }
+        }
+    }
 }
 
